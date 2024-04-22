31a1c4603e055a2c7482019bd910a35003a74565
d4vyncy <davidserrudo@gmail.com>
2024-04-22 15:15:25 -0400

    agregar slider

---
+++ mvcProyectoWeb - 04 - Slider/mvcProyectoWeb.AccesoDatos/Data/Repository/SliderRepository.cs
@@ -0,0 +1,30 @@
+﻿using mvcProyectoWeb.AccesoDatos.Data.Repository.IRepository;
+using mvcProyectoWeb.Data;
+using mvcProyectoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcProyectoWeb.AccesoDatos.Data.Repository
+{
+    public class SliderRepository : Repository<Slider>, ISliderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SliderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Slider slider)
+        {
+            var objDesdeDb = _db.Slider.FirstOrDefault(s => s.Id == slider.Id);
+            objDesdeDb.Nombre = slider.Nombre;
+            objDesdeDb.Estado = slider.Estado;
+            objDesdeDb.UrlImagen = slider.UrlImagen;
+        }
+    }
+}
