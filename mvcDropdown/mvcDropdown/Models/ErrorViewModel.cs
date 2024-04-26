d3a34ea7ee304e9db764f1ab54abf2ea1e953c89
d4vyncy <davidserrudo@gmail.com>
2024-04-26 17:33:00 -0400

    drowdow

---
+++ mvcDropdown/mvcDropdown/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcDropdown.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
